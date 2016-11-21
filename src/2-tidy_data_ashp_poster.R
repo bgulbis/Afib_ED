0c5bed2c8750bdb48200c436e39982c1e23067ae
bgulbis <brian.gulbis@gmail.com>
2016-11-21 11:09:20 -0600

    determine primary endpoint

--- src/2-tidy_data_ashp_poster.R
+++ src/2-tidy_data_ashp_poster.R
@@ -27,7 +27,7 @@ met_doses <- read_excel(met_file, sheet = "Initial Agent Boluses - Met", skip = 
 # delete columns H:Z reading in as null
 dilt_doses <- read_excel(dilt_file, sheet = "Initial Agent Boluses- Dilt", skip = 1, col_names = col_nm, col_types = col_type) %>%
     mutate(med = "diltiazem") %>%
-    filter(yes_no == "No")
+    filter(!is.na(dose_datetime))
 
 data_doses <- bind_rows(met_doses, dilt_doses)
 
@@ -49,6 +49,7 @@ options(scipen = 999)
 goal_hr <- data_vitals %>%
     filter(!is.na(hr)) %>%
     group_by(study_id) %>%
+    arrange(study_id, vital_datetime) %>%
     mutate(interval = difftime(lead(vital_datetime), vital_datetime, units = "hours"),
            interval = coalesce(interval, 0),
            duration = difftime(vital_datetime, first(vital_datetime), units = "hours"),
@@ -60,8 +61,46 @@ goal_hr <- data_vitals %>%
     mutate(goal = coalesce(goal, 0),
            percent_goal = as.numeric(goal) / as.numeric(not_goal + goal))
 
+dose_first <- data_doses %>%
+    filter(!is.na(dose)) %>%
+    group_by(study_id, med) %>%
+    arrange(study_id, dose_datetime) %>%
+    summarize(dose_datetime_first = first(dose_datetime),
+              dose_first = first(dose))
+
+vitals_doses <- data_vitals %>%
+    left_join(dose_first, by = c("study_id", "med")) %>%
+    arrange(study_id, vital_datetime) %>%
+    mutate(time_from_dose = difftime(vital_datetime, dose_datetime_first, units = "min")) %>%
+    filter(vital_datetime >= dose_datetime_first)
+
+vital_first_dose <- vitals_doses %>%
+    group_by(study_id) %>%
+    summarize(time_next_vital = first(time_from_dose),
+              hr = first(hr)) %>%
+    mutate(vital_within_30min = time_next_vital <= 30,
+           next_vital_goal = hr < 110)
+
+vital_hr_goal <- vitals_doses %>%
+    filter(hr < 110) %>%
+    group_by(study_id) %>%
+    summarize(time_dose_goal = first(time_from_dose)) %>%
+    mutate(primary = time_dose_goal <= 30)
+
 data_tidy <- data_main %>%
     left_join(goal_hr, by = "study_id") %>%
-    select(study_id, med, goal, not_goal, percent_goal, everything())
+    left_join(vital_first_dose, by = "study_id") %>%
+    left_join(vital_hr_goal, by = "study_id") %>%
+    select(study_id,
+           med, goal,
+           not_goal,
+           percent_goal,
+           time_dose_goal,
+           primary,
+           next_vital_goal,
+           time_next_vital,
+           vital_within_30min,
+           everything()) %>%
+    dmap_at("primary", ~ coalesce(.x, FALSE))
 
 write_csv(data_tidy, "data/tidy/data_ashp_poster.csv")
